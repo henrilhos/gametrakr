0384bf5a967a9a30dcdecbfff81655a0ec6c7530
Henrique de Castilhos <henrique@castilhos.dev>
2023-09-11 23:12:25 -0300

    web: tests lint ci

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,8 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import foo from "./favicon.ico"
 
 const inter = Inter({ subsets: ['latin'] })
+const bar = ''
 
 export const metadata: Metadata = {
   title: 'Create Next App',
