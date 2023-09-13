0ee37b0648198f6f8a0dec0c47febb6b8307d31b
Henrique de Castilhos <henrique@castilhos.dev>
2023-09-12 21:38:07 -0300

    web: update next config

---
+++ web/next.config.mjs
@@ -0,0 +1,33 @@
+/* eslint-disable no-process-env */
+// @ts-check
+
+import { env } from "./src/env.mjs";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: env.NODE_ENV === "production" ? "standalone" : undefined,
+  typescript: {
+    // Handled during CI
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Handled during CI
+    ignoreDuringBuilds: true,
+  },
+};
+
+/**
+ * @param {string} phase
+ * @param {{ defaultConfig: import('next').NextConfig }} options
+ */
+const nextConfigWithPlugins = async (phase, { defaultConfig }) => {
+  /* Dinamically import plugins from devDependencies to reduce bundle size */
+
+  const withBundleAnalyzer = (await import("@next/bundle-analyzer")).default({
+    enabled: env.ANALYZE,
+  });
+
+  return withBundleAnalyzer;
+};
+
+export default nextConfigWithPlugins
