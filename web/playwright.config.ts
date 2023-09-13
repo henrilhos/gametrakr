0d68947a9b90c1991bf159034ad74efacfb45070
Henrique de Castilhos <henrique@castilhos.dev>
2023-09-12 21:38:18 -0300

    web: update playwright config

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig, devices } from '@playwright/test';
+/* eslint-disable no-process-env */
+
+import { defineConfig, devices } from '@playwright/test'
+import { z } from 'zod'
 
 /**
  * Read environment variables from file.
@@ -6,30 +9,37 @@ import { defineConfig, devices } from '@playwright/test';
  */
 // require('dotenv').config();
 
+const CI = z
+  .enum(['true', 'false', '0', '1'])
+  .transform((v) => v === 'true' || v === '1')
+  .default('false')
+  .parse(process.env.CI)
+
+const baseURL = 'http://localhost:3000'
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  globalTimeout: CI ? 1000 * 60 * 10 : undefined,
   testDir: './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: !!CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
+    baseURL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-
   /* Configure projects for major browsers */
   projects: [
     {
@@ -69,9 +79,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-});
+  webServer: {
+    command: 'bun run dev',
+    url: baseURL,
+    timeout: 1000 * 60 * 5,
+    reuseExistingServer: !CI,
+  },
+})
