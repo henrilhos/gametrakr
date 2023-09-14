900d6066933f4b33b87c1c7aa908a8b9523b8009
Henrique de Castilhos <henrique@castilhos.dev>
2023-09-14 14:12:57 -0300

    feat: setup api (#6)
    * web: create next app
    
    * api: add kotlin + spring boot boilerplate
    
    * api: add .tool-version
    
    * api: add example controller
    
    * api: setup ci/cd
    
    * api: update application properties
    
    * api: update cd
    
    * api: fix cd error
    
    * api: comment unused deps
    
    * api: update port
    
    * update workflows

---
+++ api/src/main/kotlin/kr/gametra/gametrakr/GametrakrApplication.kt
@@ -0,0 +1,11 @@
+package kr.gametra.gametrakr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GametrakrApplication
+
+fun main(args: Array<String>) {
+	runApplication<GametrakrApplication>(*args)
+}
---
+++ api/src/main/kotlin/kr/gametra/gametrakr/greeting/Greeting.kt
@@ -0,0 +1,6 @@
+package kr.gametra.gametrakr.greeting
+
+data class Greeting(
+    val id: Long,
+    val context: String
+)
