397352e191787db7b905fd7a303443e448937347
Henrique de Castilhos <hello@henrique.zip>
2023-10-07 03:27:56 -0300

    chore: remove unused tables

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { authRouter } from "~/server/api/routers/auth";
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -8,7 +7,6 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   auth: authRouter,
 });
 
