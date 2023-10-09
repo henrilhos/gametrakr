9bac237e5ce77b534d3a1cdff333e7a86565f6e5
Henrique de Castilhos <hello@henrique.zip>
2023-10-09 02:19:35 -0300

    fix: base url on prod email

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -15,7 +15,7 @@ import { type AppRouter } from "~/server/api/root";
 
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 
-export const getBaseUrl = () => {
+export const getBaseSsrUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
@@ -44,7 +44,7 @@ export const api = createTRPCNext<AppRouter>({
             (opts.direction === "down" && opts.result instanceof Error),
         }),
         httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
+          url: `${getBaseSsrUrl()}/api/trpc`,
         }),
       ],
     };
