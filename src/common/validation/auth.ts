6917a7dcb29c9b4d4656a287ce4bdd377fd266ec
Henrique de Castilhos <hello@henrique.zip>
2023-10-10 16:30:51 -0300

    refactor: improve auth schemas

--- src/common/validation/auth.ts
+++ src/common/validation/auth.ts
@@ -1,38 +1,48 @@
 import { TokenType } from "@prisma/client";
 import { z } from "zod";
 
-export const signInSchema = z.object({
-  credential: z.string().describe("User email or nickname"),
-  password: z.string().describe("User password"),
-});
+// Common fields
+
+const username = z
+  .string()
+  .min(3, { message: "Nickname must be at least 3 characters" })
+  .max(24, { message: "Nickname must be not exceed 15 characters" })
+  .regex(
+    new RegExp(/^[a-zA-Z0-9._]+$/),
+    "Nickname can only have alphanumeric characters, underscores, and dots",
+  )
+  .describe("The user nickname");
+
+const email = z
+  .string()
+  .email({ message: "Invalid email" })
+  .describe("The user email");
+
+const password = z
+  .string()
+  .min(8, { message: "Password must be at least 8 characters" })
+  .regex(
+    new RegExp(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+    ),
+    "Password should include uppercase and lowercase letters, numbers, and special characters",
+  )
+  .describe("The user password");
+
+const credential = z
+  .union([email, username])
+  .describe("User email or nickname");
+
+// Schemas
+
+export const signInSchema = z.object({ credential, password });
 
 export const signUpSchema = z
   .object({
-    username: z
-      .string()
-      .min(3, { message: "Nickname must be at least 3 characters" })
-      .max(24, { message: "Nickname must be not exceed 15 characters" })
-      .regex(
-        new RegExp(/^[a-zA-Z0-9._]+$/),
-        "Nickname can only have alphanumeric characters, underscores, and dots",
-      )
-      .describe("The user nickname"),
-    email: z
-      .string()
-      .email({ message: "Invalid email" })
-      .describe("The user email"),
-    password: z
-      .string()
-      .min(8, { message: "Password must be at least 8 characters" })
-      .regex(
-        new RegExp(
-          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-        ),
-        "Password should include uppercase and lowercase letters, numbers, and special characters",
-      )
-      .describe("The user password"),
-    confirmPassword: z
-      .string()
+    username,
+    email,
+    password,
+    confirmPassword: password
       .min(8, "Passwords don't match")
       .describe("The user password (again 😁)"),
   })
@@ -46,11 +56,14 @@ export const verifyAccountSchema = z.object({
   type: z.nativeEnum(TokenType).describe("Token type"),
 });
 
-export const resendEmailSchema = z.object({
-  email: z.string().email().describe("User email"),
-});
+export const resendEmailSchema = z.object({ email });
+
+export const forgotPasswordSchema = z.object({ credential });
+
+// Types
 
 export type SignIn = z.infer<typeof signInSchema>;
 export type SignUp = z.infer<typeof signUpSchema>;
 export type VerifyAccount = z.infer<typeof verifyAccountSchema>;
 export type ResendEmail = z.infer<typeof resendEmailSchema>;
+export type ForgotPassword = z.infer<typeof forgotPasswordSchema>;
