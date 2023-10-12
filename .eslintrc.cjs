b9abd6b084c622f9c65ab9caea7bcfc2006b1951
Henrique de Castilhos <hello@henrique.zip>
2023-10-12 05:16:23 -0300

    chore: reset eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,8 @@ const config = {
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
-    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
 
     "@typescript-eslint/consistent-type-imports": [
       "warn",
