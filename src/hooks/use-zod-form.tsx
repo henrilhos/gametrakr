d12c26d666e0816a3921924cff9ff37e5416cf72
Henrique de Castilhos <hello@henrique.zip>
2023-11-13 15:20:16 -0300

    refactor: add hooks and lib folders

--- src/hooks/use-zod-form.tsx
+++ src/hooks/use-zod-form.tsx
@@ -1,18 +1,16 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm, type UseFormProps } from "react-hook-form";
+import { type ZodType } from "zod";
 
-import type { UseFormProps } from "react-hook-form";
-import type { ZodType } from "zod";
-
-export const useZodForm = <TSchema extends ZodType>(
+export function useZodForm<TSchema extends ZodType>(
   props: Omit<UseFormProps<TSchema["_input"]>, "resolver"> & {
     schema: TSchema;
   },
-) => {
+) {
   const form = useForm<TSchema["_input"]>({
     ...props,
     resolver: zodResolver(props.schema, undefined),
   });
 
   return form;
-};
+}
