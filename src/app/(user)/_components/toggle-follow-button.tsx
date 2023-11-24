eda94a476de7e663c2f82b4455616df911d844dc
Henrique de Castilhos <hello@henrique.zip>
2023-11-24 13:01:00 -0300

    Fix follow button not changing border color

--- src/app/(user)/_components/toggle-follow-button.tsx
+++ src/app/(user)/_components/toggle-follow-button.tsx
@@ -21,10 +21,11 @@ export default function ToggleFollowButton({ userId, isFollowed }: Props) {
     <button
       onClick={onClick}
       className={cn(
-        "inline-flex h-fit w-28 items-center justify-center rounded-xl bg-yellow-500 px-3 py-1 text-lg font-bold text-white transition-all hover:scale-105 hover:bg-transparent hover:text-black hover:inner-border-2 hover:inner-border-black active:scale-95 active:bg-yellow-200 active:text-yellow-500 active:inner-border-0 dark:bg-yellow-400 dark:text-black dark:hover:bg-transparent dark:hover:text-white dark:hover:inner-border-white dark:active:bg-yellow-800 dark:active:text-yellow-400",
+        "inline-flex h-fit w-28 items-center justify-center rounded-xl bg-yellow-500 px-3 py-1 text-lg font-bold text-white transition-all hover:scale-105 hover:bg-transparent hover:text-black hover:inner-border-2 hover:inner-border-black active:scale-95 active:bg-yellow-200 active:text-yellow-500 active:inner-border-0 dark:bg-yellow-400 dark:text-black dark:hover:bg-transparent dark:hover:text-white dark:active:bg-yellow-800 dark:active:text-yellow-400",
         isFollowed &&
           "before:content-['Following'] hover:bg-transparent hover:text-red-500 hover:inner-border-red-500 hover:before:content-['Unfollow'] dark:hover:bg-transparent dark:hover:text-red-400 dark:hover:inner-border-red-400",
-        !isFollowed && "before:content-['Follow']",
+        !isFollowed &&
+          "before:content-['Follow'] dark:hover:inner-border-white",
       )}
     />
   );
