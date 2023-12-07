471347bbc817a6ff794a285d83e85ee60e2b97eb
Henrique de Castilhos <hello@henrique.zip>
2023-12-07 19:40:44 -0300

    Use dynamic view height

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "~/components/navbar";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div className="flex min-h-screen w-full flex-col pb-2 md:pb-8">
+    <div className="flex min-h-[100dvh] w-full flex-col pb-2 md:pb-8">
       <Navbar />
 
       {children}
